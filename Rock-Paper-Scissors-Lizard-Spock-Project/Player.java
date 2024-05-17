33199abeb972f9723d48ebe088a38acb0039ced5
Danny Kennedy <161876494+DannyK44@users.noreply.github.com>
2024-05-17 12:50:38 -0400

    Add files via upload

---
+++ Rock-Paper-Scissors-Lizard-Spock-Project/Player.java
@@ -0,0 +1,22 @@
+/*
+ * Created on 2024-03-24
+ *
+ * Copyright (c) 2024 Nadine von Frankenberg
+ */
+
+ public final class Player {
+
+    private String name;
+
+    // Constructor for Player objects
+    public Player(String name) {
+        this.name = name;
+    }
+
+    // Getter for the names of both players
+    public String getName() {
+        return name;
+    }
+
+    
+}
